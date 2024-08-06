06ee15f8afcd94d46d63320bc26bca0227c998c5
617sam <sammridhishakya617@gmail.com>
2024-08-06 14:47:16 +0545

    First commit

---
+++ main/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    return render(request,"main/home.html",{})
+
+def about(request):
+    return render(request,"main/about.html",{})
+
+def signup(request) :
+    return render(request,"main/signup.html",{})
+def signup(request) :
+    return render(request,"main/login.html",{})
